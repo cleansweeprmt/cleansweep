97179cc739f2d1adf1a32b059791c42e4263b1ae
wansom <ochiengwarren10@gmail.com>
2024-04-11 19:02:54 +0300

    logo

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-primary rounded-lg shadow">
     <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-            <a href="https://flowbite.com/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+            <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
                 <img src="/logo.png" className="h-20" alt="Flowbite Logo" />
             </a>
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
