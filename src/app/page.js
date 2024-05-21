7aa1c732cc6c3ddbc5daaee06680f5ba7883e619
wansom <ochiengwarren10@gmail.com>
2024-05-21 06:45:25 +0300

    hero sectoion

--- src/app/page.js
+++ src/app/page.js
@@ -48,41 +48,17 @@ export default function Home() {
   };
   return (
     <div className="">
-      <main className="min-h-screen flex-col items-center justify-between bg-secondary container mx-auto px-5 lg:px-20">
-        <section className="py-12">
-          <div className=" flex flex-col md:flex-row items-center justify-center gap-5 lg:py-10">
-            <div className="lg:basis-1/2">
-              <h1 className="hero-title text-primary mb-5 text-start">
-                The next generation of connected care for the Women that need it
-                most.
+      <main className="min-h-screen flex-col items-center justify-between bg-secondary">
+        <section className="py-12  bg-cover hero bg-center lg:bg-top min-h-screen flex flex-col items-start justify-center gap-5 container mx-auto px-5 lg:px-20">
+        <h1 className="hero-title text-primary mb-5 text-start lg:max-w-[50%] lg:mt-28">
+                Your preferred partner in managing your entire HR cycle
               </h1>
-              <div className="bg-white rounded flex items-center justify-between py-2 shadow-md px-4">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
-                  />
-                </svg>
-                <hr className="w-[2px] h-10 bg-gray-300"></hr>
-                <p className="text-md text-gray-500">
-                  What can Evacare do for me?
-                </p>
-                <CtaButton />
-              </div>
-            </div>
-
-            <div>
-              <img src="/img/1.jpg" className="rounded-lg"></img>
-            </div>
-          </div>
+              <a
+           href="/consultation"
+            className="uppercase px-4 py-2 text-sm font-medium  text-center text-primary transition-colors duration-150 bg-tranparent border-2 border-primary rounded-lg active:bg-primary hover:bg-primary focus:outline-none focus:shadow-outline-purple"
+          >
+            BOOK A CONSULTATION
+          </a>
         </section>
         <section className="py-10" id="about">
           <h1 className="section-title text-dark mb-5 text-center lg:max-w-[80%] mx-auto">
