56d5205c56f19250ab5b0c28982275e9b5a51336
wansom <ochiengwarren10@gmail.com>
2024-05-28 10:26:33 +0300

    articles

--- src/app/(services)/data.js
+++ src/app/(services)/data.js
@@ -51,3 +51,29 @@ export const testimonials=[
         message:'HRfleek is a game-changer for our small business. They manage compliance and administrative tasks with ease, giving us peace of mind. Their support and expertise are invaluable.'
     }
 ]
+
+export async function getStaticProps() {
+    try {
+      const response = await fetch('https://hrfleek.com/wp-json/wp/v2/posts');
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+  
+      const posts = await response.json();
+  
+      return {
+        props: {
+          posts,
+        },
+      };
+    } catch (error) {
+      console.error('Error fetching WordPress posts:', error.message);
+      return {
+        props: {
+          posts: [],
+        },
+      };
+    }
+  }
+  
--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,9 +1,25 @@
 // pages/index.js
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import PageHeader from '../(components)/header'
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  const [loading,setLoading]=useState(true)
+  useEffect(() => {
+    setLoading(true)
+      fetch("https://hrfleek.com/wp-json/wp/v2/posts")
+      .then((response) => response.json())
+      .then(async (posts) => {
+        setLoading(false)
+        setPosts(posts);
+        console.log(posts);
+        
+      })
+      .catch((error) => setLoading(false));
+  }, []);
   return (
     <div>
       <PageHeader title={'Articles'}/>
@@ -13,60 +29,22 @@ export default function Home() {
       {/* Resource Section */}
       <div className="container mx-auto px-5 lg:px-20 py-10">
         <h2 className="text-4xl font-bold text-center">View Our <span className="text-teal-600">Articles</span></h2>
-        <div className="grid gap-8 py-16 md:grid-cols-2 lg:grid-cols-3">
-          {/* Card 1 */}
-          <div className="bg-white shadow-md rounded-lg overflow-hidden">
-            <Image src="/wansom/articles.jpg" alt="Job Rotation" width={400} height={200} className="w-full h-48 object-cover"/>
+        <div className="grid gap-8 py-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+
+          {posts.map((post,index)=>(  <div className="bg-white shadow-md rounded-lg overflow-hidden" key={index}>
+            {/* <Image src="/wansom/articles.jpg" alt="Job Rotation" width={400} height={200} className="w-full h-48 object-cover"/> */}
             <div className="p-4">
               <div className="flex items-center justify-between mb-2">
                 <p className="text-gray-600 text-sm"><i className="far fa-calendar-alt"></i> Nov 09, 2023</p>
                 <span className="bg-teal-600 text-white text-xs font-semibold px-2 py-1 rounded">HR GUIDES</span>
               </div>
               <Link href="/resources/exit-interview-tips">
-                <h3 className="text-xl font-semibold mb-2 hover:text-teal-600 transition cursor-pointer">Tips for a Successful Onboarding Program</h3>
+                <h3 className="text-xl font-semibold mb-2 hover:text-teal-600 transition cursor-pointer" dangerouslySetInnerHTML={{ __html: post?.title.rendered }}></h3>
+                <p className='text-dim text-md' dangerouslySetInnerHTML={{ __html: post?.content.rendered.slice(0, 120) }}></p>
               </Link>
-              <div className="flex items-center justify-between text-gray-600 text-sm">
-                <p><i className="far fa-user"></i> by admin</p>
-                <p><i className="far fa-comment"></i> 0</p>
-              </div>
+             
             </div>
-          </div>
-
-          {/* Card 2 */}
-          <div className="bg-white shadow-md rounded-lg overflow-hidden">
-            <Image src="/wansom/articles.jpg" alt="Exit Interview" width={400} height={200} className="w-full h-48 object-cover"/>
-            <div className="p-4">
-              <div className="flex items-center justify-between mb-2">
-                <p className="text-gray-600 text-sm"><i className="far fa-calendar-alt"></i> Oct 31, 2023</p>
-                <span className="bg-teal-600 text-white text-xs font-semibold px-2 py-1 rounded">HR GUIDES</span>
-              </div>
-              <Link href="/resources/exit-interview-tips">
-                <h3 className="text-xl font-semibold mb-2 hover:text-teal-600 transition cursor-pointer">Tips for a Successful Onboarding Program</h3>
-              </Link>
-              <div className="flex items-center justify-between text-gray-600 text-sm">
-                <p><i className="far fa-user"></i> by admin</p>
-                <p><i className="far fa-comment"></i> 0</p>
-              </div>
-            </div>
-          </div>
-
-          {/* Card 3 */}
-          <div className="bg-white shadow-md rounded-lg overflow-hidden">
-            <Image src="/wansom/articles.jpg" alt="Onboarding Program" width={400} height={200} className="w-full h-48 object-cover"/>
-            <div className="p-4">
-              <div className="flex items-center justify-between mb-2">
-                <p className="text-gray-600 text-sm"><i className="far fa-calendar-alt"></i> Oct 31, 2023</p>
-                <span className="bg-teal-600 text-white text-xs font-semibold px-2 py-1 rounded">HR GUIDES</span>
-              </div>
-              <Link href="/resources/exit-interview-tips">
-                <h3 className="text-xl font-semibold mb-2 hover:text-teal-600 transition cursor-pointer">Tips for a Successful Onboarding Program</h3>
-              </Link>
-              <div className="flex items-center justify-between text-gray-600 text-sm">
-                <p><i className="far fa-user"></i> by admin</p>
-                <p><i className="far fa-comment"></i> 0</p>
-              </div>
-            </div>
-          </div>
+          </div>))}
         </div>
 
         <div className="text-center mt-8">
