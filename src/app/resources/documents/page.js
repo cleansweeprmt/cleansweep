273e2353975cdeab56e01f184869b208172bff91
wansom <ochiengwarren10@gmail.com>
2024-06-04 16:29:16 +0300

    hello world

--- src/app/resources/documents/page.js
+++ src/app/resources/documents/page.js
@@ -78,7 +78,7 @@ useEffect(() => {
       <div className="products container mx-auto px-5 lg:px-20 py-10 flex flex-col md:flex-row gap-5 lg:gap-10">
       <div className="lg:basis-1/4">
                     {newCategories.map((i)=>(
-                        <div  className={`tab bg-gray-100 shadow-md rounded border-gray-800  border-b-[1px] border-solid px-4 py-2 text-black text-sm flex items-center justify-between ${activeCategory === i.id ? 'active bg-primary' : ''}`}  onClick={() => setActiveCategory(i.id)}>{i.name}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
+                        <div  className={`tab bg-gray-100 shadow-md rounded border-gray-800  border-b-[1px] border-solid px-4 py-3 text-black text-sm flex items-center justify-between ${activeCategory === i.id ? 'active bg-primary' : ''}`}  onClick={() => setActiveCategory(i.id)}>{i.name}<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
                         <path fillRule="evenodd" d="M15 8A7 7 0 1 0 1 8a7 7 0 0 0 14 0ZM4.75 7.25a.75.75 0 0 0 0 1.5h4.69L8.22 9.97a.75.75 0 1 0 1.06 1.06l2.5-2.5a.75.75 0 0 0 0-1.06l-2.5-2.5a.75.75 0 0 0-1.06 1.06l1.22 1.22H4.75Z" clipRule="evenodd" />
                       </svg>
                       </div>
@@ -96,7 +96,7 @@ useEffect(() => {
                 
                 <div>
                {products.map((product) => (
-              <div key={product?.id} className="flex items-center justify-between p-5 rounded shadow-lg bg-gray-50 border-b-2 border-solid border-gray-800">
+              <div key={product?.id} className="flex items-center justify-between px-5 py-3 rounded shadow-lg bg-gray-50 border-b-2 border-solid border-gray-800">
               <div className="flex items-center gap-10"> <h2>{product?.name}</h2>
                {product?.price==0&&(<p className="text-red-500 text-md me-3">FREE</p>)}
                {product?.price>0&&(<p className="text-primary text-md">KSH {product?.price}</p>)}</div>
