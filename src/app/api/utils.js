b1d75b813d30a35277ad02286b2f49fb6ddb84df
wansom <ochiengwarren10@gmail.com>
2024-06-19 10:16:02 +0300

    remove hubspot from env

--- src/app/api/utils.js
+++ src/app/api/utils.js
@@ -4,7 +4,7 @@ import nodemailer from 'nodemailer';
 
 import { Client } from '@hubspot/api-client';
 
-export const hubspotClient = new Client({ accessToken: process.env.HUBSPOT_API_KEY });
+export const hubspotClient = new Client({ accessToken:'pat-eu1-1946d52b-3476-46b4-a8f5-9b5732a35868'});
 
 
 export const woocommerce = new WooCommerceRestApi({
