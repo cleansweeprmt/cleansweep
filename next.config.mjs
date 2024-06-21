c5a1c7f9435313628a582bc07f957c3a76609f8c
wansom <ochiengwarren10@gmail.com>
2024-06-21 17:54:32 +0300

    handle redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async redirects() {
+        return [
+          // Basic redirect
+          {
+            source: '/about-us',
+            destination: '/about',
+            permanent: true,
+          },
+          // Wildcard path matching
+          {
+            source: '/contact-us',
+            destination: '/contact',
+            permanent: true,
+          },
+          {
+            source: '/blog',
+            destination: '/resources',
+            permanent: true,
+          },
+        ]
+      },
     
 };
 
