7cb032502ab026da5b31c7e72e03f1cf0bc4ffd5
wansom <ochiengwarren10@gmail.com>
2024-07-22 19:07:57 +0300

    add pop up form

--- src/components/Form/PopupForm.tsx
+++ src/components/Form/PopupForm.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+'use client'
+import  { useState } from 'react';
 import Loader from '../../app/(components)/buttonLoader';
 
 const PopupForm = ({ isOpen, onClose }) => {
