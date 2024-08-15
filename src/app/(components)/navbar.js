c7c731de832b1302d9a3098addf589260f23f598
wansom <ochiengwarren10@gmail.com>
2024-08-15 08:53:47 +0300

    add aria label to elements

--- src/app/(components)/navbar.js
+++ src/app/(components)/navbar.js
@@ -71,7 +71,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className="flex items-center gap-2">
-          <a href="https://www.linkedin.com/company/hr-fleek/?viewAsMember=true" target="blank">
+          <a href="https://www.linkedin.com/company/hr-fleek/?viewAsMember=true" target="blank" aria-label="visit our linkedin page">
           <svg className="w-6 h-6" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clip-path="url(#clip0_17_68)">
               <path d="M44.4469 0H3.54375C1.58437 0 0 1.54688 0 3.45938V44.5312C0 46.4437 1.58437 48 3.54375 48H44.4469C46.4062 48 48 46.4438 48 44.5406V3.45938C48 1.54688 46.4062 0 44.4469 0ZM14.2406 40.9031H7.11563V17.9906H14.2406V40.9031ZM10.6781 14.8688C8.39062 14.8688 6.54375 13.0219 6.54375 10.7437C6.54375 8.46562 8.39062 6.61875 10.6781 6.61875C12.9563 6.61875 14.8031 8.46562 14.8031 10.7437C14.8031 13.0125 12.9563 14.8688 10.6781 14.8688ZM40.9031 40.9031H33.7875V29.7656C33.7875 27.1125 33.7406 23.6906 30.0844 23.6906C26.3812 23.6906 25.8187 26.5875 25.8187 29.5781V40.9031H18.7125V17.9906H25.5375V21.1219H25.6312C26.5781 19.3219 28.9031 17.4188 32.3625 17.4188C39.5719 17.4188 40.9031 22.1625 40.9031 28.3313V40.9031Z" fill="white" />
@@ -83,7 +83,7 @@ const Navbar = () => {
             </defs>
           </svg>
           </a>
-          <a href="https://www.facebook.com/profile.php?id=100077643974033" target="blank">
+          <a href="https://www.facebook.com/profile.php?id=100077643974033" target="blank" aria-label="visit our facebook page">
           <svg width='24' height='24' viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g clip-path="url(#clip0_17_61)">
               <path d="M24 0C10.7453 0 0 10.7453 0 24C0 35.255 7.74912 44.6995 18.2026 47.2934V31.3344H13.2538V24H18.2026V20.8397C18.2026 12.671 21.8995 8.8848 29.9194 8.8848C31.44 8.8848 34.0637 9.18336 35.137 9.48096V16.129C34.5706 16.0694 33.5866 16.0397 32.3645 16.0397C28.4294 16.0397 26.9088 17.5306 26.9088 21.4061V24H34.7482L33.4013 31.3344H26.9088V47.8243C38.7926 46.3891 48.001 36.2707 48.001 24C48 10.7453 37.2547 0 24 0Z" fill="white" />
@@ -96,12 +96,12 @@ const Navbar = () => {
           </svg>
          
           </a>
-          <a href="https://x.com/HRFleek" target="blank">
+          <a href="https://x.com/HRFleek" target="blank" aria-label="visit our twitter page">
           <svg className="w-6 h-6" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M36.6526 3.8078H43.3995L28.6594 20.6548L46 43.5797H32.4225L21.7881 29.6759L9.61989 43.5797H2.86886L18.6349 25.56L2 3.8078H15.9222L25.5348 16.5165L36.6526 3.8078ZM34.2846 39.5414H38.0232L13.8908 7.63406H9.87892L34.2846 39.5414Z" fill="white" />
           </svg>
           </a>
-          <a href="https://www.tiktok.com/@hrfleek_kenya?_t=8mnygT77gwv&_r=1" target="blank">  <svg className="h-6 w-6" fill="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <a href="https://www.tiktok.com/@hrfleek_kenya?_t=8mnygT77gwv&_r=1" target="blank" aria-label="visit our tiktok page">  <svg className="h-6 w-6" fill="white" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path d="M19.589 6.686a4.793 4.793 0 0 1-3.77-4.245V2h-3.445v13.672a2.896 2.896 0 0 1-5.201 1.743l-.002-.001.002.001a2.895 2.895 0 0 1 3.183-4.51v-3.5a6.329 6.329 0 0 0-5.394 10.692 6.33 6.33 0 0 0 10.857-4.424V8.687a8.182 8.182 0 0 0 4.773 1.526V6.79a4.831 4.831 0 0 1-1.003-.104z"/>
             </svg></a>
           <a href="https://www.instagram.com/hrfleek/" target="blank">
@@ -118,7 +118,7 @@ const Navbar = () => {
       </div>
       <div className="container mx-auto px-5 lg:px-20 flex items-center justify-between lg:shadow-lg lg:rounded-b-lg lg:bg-white p-3   z-10 lg:w-full">
         <div className="logo">
-          <a href="/" className="nav-logo">
+          <a href="/" className="nav-logo" aria-label="Go back to Homepage">
             <Image src="/logo.png" alt="logo" width={200} height={70} />
           </a>
         </div>
