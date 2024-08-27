8945a1510c69fa5a7a3207b342836696de2ab2a6
wansom <ochiengwarren10@gmail.com>
2024-08-27 14:09:13 +0300

    update footer

--- src/app/(components)/footer.js
+++ src/app/(components)/footer.js
@@ -4,7 +4,7 @@ import { FloatingWhatsApp } from 'react-floating-whatsapp'
 import Logo from '../assets/logo.png'
 const Footer = () => {
     return ( 
-        <footer className="bg-gray-800 rounded-top-lg shadow">
+        <footer className="bg-primary rounded-top-lg shadow">
     <FloatingWhatsApp   phoneNumber="+254741511183"
         accountName="HRFLEEK"
         avatar='./logo.png'
@@ -14,9 +14,9 @@ const Footer = () => {
     <div className="container mx-auto px-5 lg:px-20 py-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
             <a href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
-                <img src="/logo.png" className="h-20 footer-logo" alt="Flowbite Logo" />
+                <img src="/logo.png" className="h-20 footer-logo" alt="Logo" />
             </a>
-            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-primary sm:mb-0">
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-white sm:mb-0">
                
                 <li>
                     <a href="/contact" className="hover:underline me-4 md:me-6">Privacy Policy</a>
@@ -28,7 +28,7 @@ const Footer = () => {
             </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-        <span className="block text-sm text-primary text-center">© {new Date().getFullYear()} <a href="#" className="hover:underline">HRFLEEK</a>. All Rights Reserved.</span>
+        <span className="block text-sm text-white text-center">© {new Date().getFullYear()} <a href="#" className="hover:underline">SpruceItUp</a>. All Rights Reserved.</span>
     </div>
 </footer>
      );
