ffee6188be179c23e7dc860c6ac4adfb248b2735
wansom <ochiengwarren10@gmail.com>
2024-10-15 18:49:25 +0300

    update contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -71,7 +71,7 @@ const Page = () => {
           {/* Left Section */}
           <div className="w-full lg:w-1/2 px-4 hidden lg:block">
           <div className="space-y-3">
-          <div className="flex flex-col items-center justify-center border-2 border-solid border-gray-100 shadow-ld rounded-lg p-10 text-center gap-4">
+          <div className="flex  items-center justify-center border-2 border-solid border-gray-100 shadow-ld rounded-lg p-10 text-center gap-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -93,14 +93,14 @@ const Page = () => {
             </svg>
             <p>Dorus Rikerskplein- Hague</p>
           </div>
-          <div className="flex flex-col items-center justify-center border-2 border-solid border-gray-100 shadow-ld rounded-lg p-10 text-center gap-4">
+          <div className="flex items-center justify-center border-2 border-solid border-gray-100 shadow-ld rounded-lg p-10 text-center gap-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
             </svg>
 
             <p> +31630665434</p>
           </div>
-          <div className="flex flex-col items-center justify-center border-2 border-solid border-gray-100 shadow-ld rounded-lg p-10 text-center gap-4">
+          <div className="flex items-center justify-center border-2 border-solid border-gray-100 shadow-ld rounded-lg p-10 text-center gap-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
             </svg>
@@ -116,24 +116,15 @@ const Page = () => {
           {/* Right Section */}
           <div className="w-full lg:w-1/2 px-4 mt-6 lg:mt-0">
           <form onSubmit={handleSubmit} className="bg-white p-8 shadow rounded-lg">
-      <div className="grid grid-cols-2 gap-4 mb-4">
         <input
           type="text"
           name="firstName"
           value={formData.firstName}
           onChange={handleChange}
           placeholder="First Name"
-          className="border p-3 rounded outline-none focus:border-blue-500"
+          className="border p-3 rounded outline-none focus:border-blue-500 w-full mb-4"
         />
-        <input
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          onChange={handleChange}
-          placeholder="Last Name"
-          className="border p-3 rounded outline-none focus:border-blue-500"
-        />
-      </div>
+        
       <div className="grid grid-cols-2 gap-4 mb-4">
         <input
           type="text"
@@ -152,14 +143,6 @@ const Page = () => {
           className="border p-3 rounded outline-none focus:border-blue-500"
         />
       </div>
-      <input
-        type="text"
-        name="company"
-        value={formData.company}
-        onChange={handleChange}
-        placeholder="Company/Organization Name"
-        className="border p-3 rounded outline-none focus:border-blue-500 w-full mb-4"
-      />
       <textarea
         name="details"
         value={formData.details}
