236c0458e0f1de2a60b63b09461fb6cc6aeaac63
wansom <ochiengwarren10@gmail.com>
2024-10-15 18:44:17 +0300

    update contact us page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -59,7 +59,18 @@ const Page = () => {
       <PageHeader title={'Contact Us'} />
       <ToastContainer />
       <section className="container mx-auto px-5 lg:px-20 py-10">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+      
+
+
+
+      </section>
+
+      <section className="container mx-auto px-5 lg:px-20 py-10">
+        <div className="flex flex-col-reverse lg:flex-row justify-between lg:gap-10">
+
+          {/* Left Section */}
+          <div className="w-full lg:w-1/2 px-4 hidden lg:block">
+          <div className="space-y-3">
           <div className="flex flex-col items-center justify-center border-2 border-solid border-gray-100 shadow-ld rounded-lg p-10 text-center gap-4">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -80,34 +91,23 @@ const Page = () => {
                 d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
               />
             </svg>
-            <p>I&M Bank House, 3rd Floor, 2nd Ngong Avenue, Nairobi</p>
+            <p>Dorus Rikerskplein- Hague</p>
           </div>
           <div className="flex flex-col items-center justify-center border-2 border-solid border-gray-100 shadow-ld rounded-lg p-10 text-center gap-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
             </svg>
 
-            <p>+254741511183</p>
+            <p> +31630665434</p>
           </div>
           <div className="flex flex-col items-center justify-center border-2 border-solid border-gray-100 shadow-ld rounded-lg p-10 text-center gap-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 9v.906a2.25 2.25 0 0 1-1.183 1.981l-6.478 3.488M2.25 9v.906a2.25 2.25 0 0 0 1.183 1.981l6.478 3.488m8.839 2.51-4.66-2.51m0 0-1.023-.55a2.25 2.25 0 0 0-2.134 0l-1.022.55m0 0-4.661 2.51m16.5 1.615a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V8.844a2.25 2.25 0 0 1 1.183-1.981l7.5-4.039a2.25 2.25 0 0 1 2.134 0l7.5 4.039a2.25 2.25 0 0 1 1.183 1.98V19.5Z" />
             </svg>
 
-            <p>info@spruceitup.com</p>
+            <p>info@spruceitup.nl</p>
           </div>
         </div>
-
-
-
-      </section>
-
-      <section className="container mx-auto px-5 lg:px-20 py-10">
-        <div className="flex flex-col-reverse lg:flex-row justify-between lg:gap-10">
-
-          {/* Left Section */}
-          <div className="w-full lg:w-1/2 px-4 hidden lg:block">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.800665020861!2d36.808691074965616!3d-1.2941083986935926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f11e9e9f172db%3A0xde236816f452452b!2sI%26M%20Bank%202nd%20Ngong%20Avenue%20Branch%2C%20Ngong%20Ave%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1717142028581!5m2!1sen!2ske" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
           <div className="w-full lg:w-1/2 px-4 block lg:hidden">
           <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.800665020861!2d36.808691074965616!3d-1.2941083986935926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f11e9e9f172db%3A0xde236816f452452b!2sI%26M%20Bank%202nd%20Ngong%20Avenue%20Branch%2C%20Ngong%20Ave%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1717142028581!5m2!1sen!2ske" width="330" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
